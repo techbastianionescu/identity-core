c0521c5a0697d7cd42b9bd83ca6e534bcdbc92de
techbastianionescu <techbastiann@gmail.com>
2026-03-14 19:21:15 +0100

    feat: add database connection, session factory and declarative base

---
+++ backend/app/database.py
@@ -0,0 +1,10 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker,DeclarativeBase
+import os
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+class Base(DeclarativeBase):
+    pass
